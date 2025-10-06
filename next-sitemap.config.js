7fc7832a14af990279f8ef545288e03fab6e1734
Mohammad-Afzal786 <affutech1@gmail.com>
2025-10-06 17:50:32 +0530

    update

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteUrl: "https://mohallabazaar.vercel.app",
-  generateRobotsTxt: true,
-  additionalPaths: async () => [
-    { loc: "/download", lastmod: new Date().toISOString() },
-   
-  ],
-};
