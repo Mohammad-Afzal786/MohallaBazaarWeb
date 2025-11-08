5bd8a5894414caedcae67933b1e08311473f53f0
Mohammad-Afzal786 <affutech1@gmail.com>
2025-11-09 03:50:35 +0530

    update

--- app/api/download-apk/route.js
+++ app/api/download-apk/route.js
@@ -2,22 +2,5 @@ export async function GET() {
   const githubApkUrl =
     "https://github.com/Mohammad-Afzal786/MohallaBazaarWeb/releases/download/v1.0.2/MohallaBazaar.apk";
 
-  try {
-    const response = await fetch(githubApkUrl);
-
-    if (!response.ok) {
-      return new Response("File not found on MohallaBazaar", { status: 404 });
-    }
-
-    return new Response(response.body, {
-      status: 200,
-      headers: {
-        "Content-Type": "application/vnd.android.package-archive",
-        "Content-Disposition": "attachment; filename=MohallaBazaar.apk",
-      },
-    });
-  } catch (error) {
-    console.error("❌ Error fetching APK:", error);
-    return new Response("Server error while fetching file", { status: 500 });
-  }
+  return Response.redirect(githubApkUrl, 302);
 }
